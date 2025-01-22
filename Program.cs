8d06e391c6d797dc343d5669d4816c33cfd51b0e
Luiz Felipe <luizfelipesiqueira83@gmail.com>
2025-01-21 23:16:20 -0300

    Adição em formato de tempo

--- Program.cs
+++ Program.cs
@@ -10,20 +10,34 @@ namespace StopWatch
 
             Menu();
 
-
-
         }
+
 
 
         static void Menu()
         {
-            Console.Clear();
-            Console.WriteLine("Bem vindo ao cronomêtro 'caseiro' ");
-
             Console.WriteLine("Escolha uma opção: ");
+            Console.WriteLine("S = Segndos 10s => 10 segundos");
+            Console.WriteLine("m = Minutos 1m => 1 minuto");
+            Console.WriteLine("0 = Sair");
+            string data = Console.ReadLine().ToLower();
+            char type = char.Parse(data.Substring(data.Length - 1, 1));
+
+
+            int time = int.Parse(data.Substring(0, data.Length - 1));
+
+            if (type == 'm')
+                time *= 60;
+
+            if (time == 0)
+                Environment.Exit(0);
+
+            Console.WriteLine("---------------------------");
+            Console.WriteLine("Agora Escolha uma opção: ");
             Console.WriteLine("1 - Descendente");
             Console.WriteLine("2 - Crescente");
             Console.WriteLine("3 - Sair");
+
             short res;
             while (!short.TryParse(Console.ReadLine(), out res) || res < 1 || res > 3)
             {
@@ -32,53 +46,42 @@ namespace StopWatch
 
             switch (res)
             {
-                case 1: Descending(); break;
-                case 2: Ascending(); break;
+                case 1: Descending(time); break;
+                case 2: Ascending(time); break;
                 case 3: Console.Clear(); Environment.Exit(0); break;
                 default: Console.Clear(); Environment.Exit(0); break;
             }
+
+
         }
 
-        static void Descending()
+        static void Descending(int time)
         {
 
-            int seconds = 0;
-            Console.WriteLine("CRONOMETRO DECRESCENTE");
-            Console.WriteLine("Digite quanto tempo deseja esperar: ");
-            while (!int.TryParse(Console.ReadLine(), out seconds))
-            {
-                Console.WriteLine("Digite um valor válido");
-            }
-
-            for (int i = seconds; i >= 0; i--)
+            while (time > 0)
             {
                 Console.Clear();
-                Console.WriteLine($"Tempo restante: {i}");
-                System.Threading.Thread.Sleep(1000);
+                time--;
+                Console.WriteLine($"Tempo restante: {time}");
+                Thread.Sleep(1000);
             }
 
             Console.WriteLine("Fim do cronometro");
             Console.WriteLine("------------------");
-            System.Threading.Thread.Sleep(2000);
+            Thread.Sleep(2000);
             Menu();
         }
 
-        static void Ascending()
+        static void Ascending(int time)
         {
-            int correntTime = 0;
+
             int seconds = 0;
 
-            Console.WriteLine("CRONOMETRO CRESCENTE");
-            Console.WriteLine("Digite até quantos segundos desja contar: ");
-            while (!int.TryParse(Console.ReadLine(), out correntTime))
-            {
-                Console.WriteLine("Digite um valor válido");
-            }
-
-            for (int i = seconds; i < correntTime; i++)
+            while (seconds < time)
             {
                 Console.Clear();
-                Console.WriteLine($"Tempo: {i}");
+                seconds++;
+                Console.WriteLine($"Tempo: {seconds}");
                 System.Threading.Thread.Sleep(1000);
             }
 
